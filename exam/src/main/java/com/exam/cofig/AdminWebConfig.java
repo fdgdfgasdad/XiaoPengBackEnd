46ea4b7d1da40dd4628054bf09f22c7c1dfd925e
Dyrrachion <santatsuikwan@protonmail.com>
2022-06-23 20:55:31 +0800

    Auth added

--- exam/src/main/java/com/exam/cofig/AdminWebConfig.java
+++ exam/src/main/java/com/exam/cofig/AdminWebConfig.java
@@ -1,21 +1,21 @@
-//package com.exam.cofig;
-//
-//import com.exam.interceptor.LoginHandleInterceptor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@Slf4j
-//public class AdminWebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//
-//        registry.addInterceptor(new LoginHandleInterceptor())
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/", "/login", "/register");
-//
-//    }
-//}
+package com.exam.cofig;
+
+import com.exam.interceptor.LoginHandleInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class AdminWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new LoginHandleInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/register");
+
+    }
+}
--- exam/src/main/java/com/exam/interceptor/LoginHandleInterceptor.java
+++ exam/src/main/java/com/exam/interceptor/LoginHandleInterceptor.java
@@ -1,22 +1,27 @@
 package com.exam.interceptor;
 
 import com.exam.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class LoginHandleInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //String token
         String token = request.getHeader("token");
+        log.info(token);
         if (JwtUtil.verify(token))
         {
+            log.info("verify success");
             return true;
         }
+        log.info("verify failed");
         request.setAttribute("msg","登陆出错");
         request.getRemoteHost();
 
